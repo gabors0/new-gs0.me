071ef1b39a8e1399aadd1adb92e36fab5d923d15
gabors0 <gabors0@proton.me>
2026-04-18 12:57:01 +0200

    i forgor a file

---
+++ src/lib/SectionTitle.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<div class="flex items-center">
+  <span
+    aria-hidden="true"
+    class="flex-1 border-t border-white/30"
+  ></span>
+  <span class="shrink-0 px-3 py-3 text-lg opacity-70 text-white/90"
+    >{title}</span
+  >
+  <span
+    aria-hidden="true"
+    class="flex-1 border-t border-white/30"
+  ></span>
+</div>
